94e4c1fa6d6eebfd730ff929d575fbb5600a2e70
昭召 <lz96297@alibaba-inc.com>
2017-12-14 22:29:04 +0800

    Initial Project

---
+++ Bygones/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Bygones
+//
+//  Created by lizhao on 2017/12/14.
+//  Copyright © 2017年 lizhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Bygones/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Bygones
+//
+//  Created by lizhao on 2017/12/14.
+//  Copyright © 2017年 lizhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
